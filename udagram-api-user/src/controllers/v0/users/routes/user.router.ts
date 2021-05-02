e0a599d95037a0735b4fa9f2f7b3ec4d2723281f
rowlandekemezie <rowland.ekemezie@gmail.com>
2021-05-02 23:56:04 +0800

    initial commit

---
+++ udagram-api-user/src/controllers/v0/users/routes/user.router.ts
@@ -0,0 +1,21 @@
+import {Router, Request, Response} from 'express';
+
+import {User} from '../models/User';
+import {AuthRouter} from './auth.router';
+
+const router: Router = Router();
+
+router.use('/auth', AuthRouter);
+
+router.get('/', async (_: Request, res: Response) => {
+  const users = await User.findAll();
+  res.send(users)
+});
+
+router.get('/:id', async (req: Request, res: Response) => {
+  const {id} = req.params;
+  const item = await User.findByPk(id);
+  res.send(item);
+});
+
+export const UserRouter: Router = router;
